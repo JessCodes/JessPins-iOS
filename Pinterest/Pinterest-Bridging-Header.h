efe0fc7822b08d5a40008eab4a973a3577511a0e
JessCodes <jessicafellison@gmail.com>
2017-03-23 17:53:03 -0700

    Finish 'Getting Started' part of challenge.

---
+++ Pinterest/Pinterest-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Pinterest-Bridging-Header.h
+//  Pinterest
+//
+//  Created by Jessica Ellison on 3/23/17.
+//  Copyright © 2017 JessCodes. All rights reserved.
+//
+
+#ifndef Pinterest_Bridging_Header_h
+#define Pinterest_Bridging_Header_h
+
+#import <PinterestSDK/PinterestSDK.h>
+
+#endif /* Pinterest_Bridging_Header_h */
